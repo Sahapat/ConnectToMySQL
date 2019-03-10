6ed0c9d34378f5d2c386aad150ef3204ff2e6064
Sahapat <sahapat.starcaft@gmail.com>
2019-03-10 11:21:18 +0700

    Add project in class

---
+++ server.js
@@ -0,0 +1,45 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "developer",
+    password: "Rx123456",
+    database: 'gameimplement1'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log('error connecting', err.stack);
+        return;
+    }
+    console.log('connected as id ', connection.threadId);
+});
+
+console.log('server is running');
+
+var player = {
+    firstname: 'Abc',
+    lastname: 'Xyz',
+    playername: 'abx',
+    password: '123456'
+};
+
+connection.query('INSERT INTO player SET ?', player,
+    function (err, result) {
+        if (err) throw err;
+
+        console.log(result);
+    });
+
+connection.query('SELECT * FROM player',
+    function (err, rows, fields) {
+        if (err) throw err;
+
+        for (var i in rows) {
+            console.log(rows[i].playername)
+        }
+    });
+
+connection.end(function (err){
+    console.log("Terminate Connection");
+})
